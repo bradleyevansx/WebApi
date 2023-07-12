030539aab6c8494ae0c4fdd8a7d01755998463e9
Brad <bradleyevans.x@gmail.com>
2023-07-12 10:16:06 -0400

    Changing Client String

--- WebAPI.Repository/CosmosConnectionManager.cs
+++ WebAPI.Repository/CosmosConnectionManager.cs
@@ -9,7 +9,7 @@ public class CosmosConnectionManager
 
     public CosmosConnectionManager()
     {
-        ClientConnection = new CosmosClient("AccountEndpoint=https://obito-rip.documents.azure.com:443/;AccountKey=mZm47VxAmbhvNjRgT8CDx03kLmdMfotNW4gyptOfTB4Rl7kxBnzCwXTtcDjh6Cl8tgTuoI56HXKzACDb7QFGjw==;");
+        ClientConnection = new CosmosClient("https://obito-rip.documents.azure.com:443/", "mZm47VxAmbhvNjRgT8CDx03kLmdMfotNW4gyptOfTB4Rl7kxBnzCwXTtcDjh6Cl8tgTuoI56HXKzACDb7QFGjw==");
     }
 
     public Container CreateConnection(string containerId)
