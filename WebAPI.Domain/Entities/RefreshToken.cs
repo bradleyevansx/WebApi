847d0001c4402644bdd216ec85d185a7f9fd4526
Brad <bradleyevans.x@gmail.com>
2023-07-12 09:48:41 -0400

    Fixes

--- WebAPI.Domain/Entities/RefreshToken.cs
+++ WebAPI.Domain/Entities/RefreshToken.cs
@@ -3,7 +3,7 @@ namespace WebAPI.Domain.Models;
 public class RefreshToken : Entity
 {
     public override string? PartitionKey => id;
-    public string UserId { get; set; }
+    public string? UserId { get; set; }
     public DateTime ExpirationDateTime { get; set; }
     public bool IsValid { get; set; }
 }
